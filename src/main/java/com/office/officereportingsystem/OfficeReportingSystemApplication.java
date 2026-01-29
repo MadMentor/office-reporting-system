49b71ffc68843f067fb2e2f0a95dc97a06ea5dad
Faithful Crow 778 <sumitsainju@gmail.com>
2026-01-29 14:41:48 +0545

    Initialize Spring Boot project with base structure

---
+++ src/main/java/com/office/officereportingsystem/OfficeReportingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.office.officereportingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeReportingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeReportingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/office/officereportingsystem/OfficeReportingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.office.officereportingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeReportingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
